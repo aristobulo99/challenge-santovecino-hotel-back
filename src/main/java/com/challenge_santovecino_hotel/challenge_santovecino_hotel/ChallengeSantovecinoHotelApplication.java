c47ab6d5fef02f5e5e979fc4db0ab704f1859e93
juniorespriella99 <juniorespriella99@gmail.com>
2024-11-27 18:41:17 -0500

    fix: init project

---
+++ src/main/java/com/challenge_santovecino_hotel/challenge_santovecino_hotel/ChallengeSantovecinoHotelApplication.java
@@ -0,0 +1,13 @@
+package com.challenge_santovecino_hotel.challenge_santovecino_hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeSantovecinoHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeSantovecinoHotelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/challenge_santovecino_hotel/challenge_santovecino_hotel/ChallengeSantovecinoHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge_santovecino_hotel.challenge_santovecino_hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeSantovecinoHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
