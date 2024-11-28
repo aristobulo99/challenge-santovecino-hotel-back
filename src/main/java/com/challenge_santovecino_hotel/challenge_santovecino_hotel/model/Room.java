23c4c9982a4359c13840be3dea9caa93b45c5f12
juniorespriella99 <juniorespriella99@gmail.com>
2024-11-27 19:50:56 -0500

    fix: creacion de la entidad habitacion

--- src/main/java/com/challenge_santovecino_hotel/challenge_santovecino_hotel/model/Room.java
+++ src/main/java/com/challenge_santovecino_hotel/challenge_santovecino_hotel/model/Room.java
@@ -1,4 +1,35 @@
 package com.challenge_santovecino_hotel.challenge_santovecino_hotel.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "room")
+@Getter
+@Setter
 public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false, length = 20)
+    private String name;
+
+    @Column(name = "description", nullable = false, length = 500)
+    private String description;
+
+    @Column(name = "state", nullable = false)
+    private Boolean state;
+
+    public Room() {
+    }
+
+    public Room(Long id, String name, String description, Boolean state) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.state = state;
+    }
 }
